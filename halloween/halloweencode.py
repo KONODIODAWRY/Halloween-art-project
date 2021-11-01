620631b3bc267a24d631e647569344605fb7e20e
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2021-11-01 15:38:52 -0700

    Update halloweencode.py

--- halloween/halloweencode.py
+++ halloween/halloweencode.py
@@ -4,12 +4,23 @@ wn = trtl.Screen()
 gifs=['ghost.gif', 'skull.gif', 'smile.gif']
 for i in gifs:
 	wn.addshape(i)
-t=trtl
+t=trtl.Turtle()
+u=trtl.Turtle()
+u.penup()
+u.goto(-250,250)
 n=0
+s=1
+font_setup=("Arial", 20, "normal")
+def score():
+  global s
+  u.clear()
+  u.write(s, font=font_setup)
+  s+=1
 def spin(x,y):
   t.penup()
   t.circle(40)
 wn.bgpic("fog.gif")
 t.shape(random.choice(gifs))
 t.onclick(spin)
+u.onclick(score())
 wn.mainloop()
